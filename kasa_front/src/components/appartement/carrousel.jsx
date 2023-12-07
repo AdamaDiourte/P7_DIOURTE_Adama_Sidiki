29b01b62716c676146b86362fe956426114066ef
Adama Sidiki DIOURTE <adamasidikidiourte@hotmail.fr>
2023-12-07 23:34:03 +0100

    MAJ_du_08/12/2023

--- kasa_front/src/components/appartement/carrousel.jsx
+++ kasa_front/src/components/appartement/carrousel.jsx
@@ -8,9 +8,12 @@ import appartData from '../../data/data.js';
 import "../../styles/fichierstyles/carrousel.scss";
 
 function Carrousel() {
+  // État pour stocker les images de l'appartement
   const [pictures, setPictures] = useState([]);
+  // Extraction de l'ID de l'appartement à partir de l'URL
   const { appartId } = useParams();
 
+  // Effet pour charger les images de l'appartement
   useEffect(() => {
     const foundAppart = appartData.find(appart => appart.id === appartId);
     if (foundAppart && foundAppart.pictures) {
@@ -18,10 +21,12 @@ function Carrousel() {
     }
   }, [appartId]);
 
+  // Affichage d'un indicateur de chargement si aucune image n'est trouvée
   if (pictures.length === 0) {
     return <div>Loading...</div>;
   }
 
+  // Fonction pour définir les styles des flèches de navigation
   const arrowStyles = (left = true) => ({
     position: 'absolute',
     zIndex: 2,
@@ -36,6 +41,7 @@ function Carrousel() {
     outline: 'none'
   });
 
+  // Styles pour la légende sous chaque image
   const legendStyles = {
     color: 'white',
     fontSize: '1.2rem',
@@ -46,6 +52,7 @@ function Carrousel() {
   return (
     <div className="carrousel-container">
       <Carousel 
+        // Propriétés du composant Carousel
         showArrows={pictures.length > 1}
         showStatus={false}
         showThumbs={false}
@@ -57,6 +64,7 @@ function Carrousel() {
         emulateTouch={true}
         dynamicHeight={true}
         renderArrowPrev={(onClickHandler, hasPrev) =>
+          // Rendu conditionnel de la flèche précédente
           hasPrev && (
             <button type="button" onClick={onClickHandler} style={arrowStyles(true)}>
               <FontAwesomeIcon icon={faChevronLeft} size="3x" />
@@ -64,22 +72,25 @@ function Carrousel() {
           )
         }
         renderArrowNext={(onClickHandler, hasNext) =>
+          // Rendu conditionnel de la flèche suivante
           hasNext && (
             <button type="button" onClick={onClickHandler} style={arrowStyles(false)}>
               <FontAwesomeIcon icon={faChevronRight} size="3x" />
             </button>
           )
         }
-        renderIndicator={(onClickHandler, isSelected, index) => (
+        renderIndicator={(onClickHandler, isSelected, index) =>
+          // Rendu des indicateurs de sélection
           <button
             type="button"
             onClick={onClickHandler}
             aria-label={`Image ${index + 1}`}
             style={{ background: isSelected ? '#ff6060' : 'transparent' }}
           />
-        )}
+        }
       >
         {pictures.map((pic, index) => (
+          // Rendu de chaque image dans le carrousel
           <div key={index} className="slide-container">
             <img src={pic} alt={`Image de l'appartement ${index + 1}`} />
             {pictures.length > 1 && <p className="legend" style={legendStyles}>{`${index + 1} / ${pictures.length}`}</p>}
--- kasa_front/src/components/collapse/collapse.jsx
+++ kasa_front/src/components/collapse/collapse.jsx
@@ -1,30 +1,26 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import "../../styles/fichierstyles/collapse.scss";
 
 function Collapse({ title, content }) {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleCollapse = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    };
 
-  // Styles conditionnels pour le conteneur du collapse
-  const containerStyles = {
-    backgroundColor: isOpen ? '#f4efef' : 'transparent', // Couleur de fond appliquée seulement si ouvert
-    boxShadow: isOpen ? '0 2px 4px rgba(0, 0, 0, 0.1)' : 'none' // Box-shadow appliqué seulement si ouvert
-  };
-
-  return (
-    <div className='collapse-container' style={containerStyles}>
-      <div className='collapse-button' onClick={toggleCollapse}>
-        <button>{title}</button>
-        <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronUp} />
-      </div>
-      {isOpen && <div className='collapse-content'>{content}</div>}
-    </div>
-  );
+    return (
+        <div className={`collapse-container ${isOpen ? 'open' : ''}`}>
+            <div className={`collapse-button ${isOpen ? 'open' : ''}`} onClick={toggleCollapse}>
+                <button>{title}</button>
+                <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+            <div className={`collapse-content ${isOpen ? 'open' : ''}`}>
+                {content}
+            </div>
+        </div>
+    );
 }
 
 export default Collapse;
